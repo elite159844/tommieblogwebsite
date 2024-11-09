1053c24a4ef7cf4a09610c56969eacc64f3d6b69
elite159844 <tommietra@yahoo.com>
2024-11-08 19:20:56 -0500

    push changes

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://eliteblogwebsite.netlify.app/", // replace this with your deployed domain
+  website: "https://eliteblogwebsite.pages.dev/", // replace this with your deployed domain
   author: "Tommie",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
