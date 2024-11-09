410a55e6eb1f087dd99d0989387ff3ad72b9fd9a
elite159844 <tommietra@yahoo.com>
2024-11-08 19:27:02 -0500

    push changes

--- src/config.ts
+++ src/config.ts
@@ -42,19 +42,19 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
     href: "",
     linkTitle: `${SITE.title} on Instagram`,
-    active: true,
+    active: false,
   },
   {
     name: "LinkedIn",
     href: "",
     linkTitle: `${SITE.title} on LinkedIn`,
-    active: true,
+    active: false,
   },
   {
     name: "Mail",
